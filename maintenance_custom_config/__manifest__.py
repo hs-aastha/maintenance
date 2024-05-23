f7c982cbea1e1bc75c34d46babfbd498affb4f4a
hs-arjun <arjun.chandra.baidya@gmail.com>
2024-05-23 14:36:40 +0600

    fix error menu iteam plase under action

--- maintenance_custom_config/__manifest__.py
+++ maintenance_custom_config/__manifest__.py
@@ -1,16 +1,28 @@
+
 {
     'name': 'Maintenance Custom Config',
-    'version': '17.0.0',
-    'summary': 'Custom configurations for the Maintenance app',
+    'version': '17.0.1.0.0',
+    'summary': "Custom configurations for the Maintenance app",
+    'author': "Hundred Solutions",
+    'maintainer': 'Hundred Solutions',
+    'company': "Hundred Solutions",
+    'website': 'https://www.hundredsolutions.com/',
     'category': 'Maintenance',
-    'author': 'Your Name',
-    'depends': ['maintenance'],
+    'description': """
+    Helps You To work odoo and tripletex.
+    """,
+    'depends': [
+        'maintenance',
+    ],
     'data': [
+        # 'security/ir.model.access.csv',
         'views/maintenance_attribute_views.xml',
         'views/maintenance_measurement_views.xml',
         'views/maintenance_transform_views.xml',
         'views/maintenance_metric_views.xml',
     ],
+    # 'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
     'installable': True,
-    'application': False,
+    'application': True,
 }
