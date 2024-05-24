0e8a19fefe45ff6b2ed160b46dea1bb0d1fe0e1a
aastha <aastha.a@hundredsolutions.com>
2024-05-24 16:31:09 +0530

    manifest file updated to add access rights file

--- maintenance_custom_config/__manifest__.py
+++ maintenance_custom_config/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': 'Maintenance Custom Config',
     'version': '17.0.1.0.0',
-    'summary': "Custom configurations for the Maintenance app",
+    'summary': "Custom configurations for the Maintenance app to integrate AWS IoT Sitewise",
     'author': "Hundred Solutions",
     'maintainer': 'Hundred Solutions',
     'company': "Hundred Solutions",
@@ -15,7 +15,7 @@
         'maintenance',
     ],
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/maintenance_attribute_views.xml',
         'views/maintenance_measurement_views.xml',
         'views/maintenance_transform_views.xml',
