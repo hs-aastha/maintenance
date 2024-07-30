570cc7d2a009f9608acccfe87766e65f35479a88
hs-aastha <aastha.a@hundredsolutions.com>
2024-07-30 13:10:32 +0530

    Reverting to previous version

--- sitewise_integration/models/__init__.py
+++ sitewise_integration/models/__init__.py
@@ -3,4 +3,3 @@ from . import maintenance_attribute
 from . import maintenance_measurement
 from . import maintenance_transform
 from . import maintenance_metric
-from . import maintenance_equipment_line
--- sitewise_integration/models/maintenance_equipment.py
+++ sitewise_integration/models/maintenance_equipment.py
@@ -1,9 +1,11 @@
+# /path/to/your/addons/sitewise_integration/models/maintenance_equipment.py
+
 from odoo import models, fields
 
 class MaintenanceEquipment(models.Model):
     _inherit = 'maintenance.equipment'
 
-    attribute_line_ids = fields.One2many('maintenance.attribute.line', 'equipment_id', string='Attributes')
-    measurement_line_ids = fields.One2many('maintenance.measurement.line', 'equipment_id', string='Measurements')
-    transform_line_ids = fields.One2many('maintenance.transform.line', 'equipment_id', string='Transforms')
-    metric_line_ids = fields.One2many('maintenance.metric.line', 'equipment_id', string='Metrics')
+    attribute_ids = fields.One2many('maintenance.attribute', 'equipment_id', string='Attributes')
+    measurement_ids = fields.One2many('maintenance.measurement', 'equipment_id', string='Measurements')
+    transform_ids = fields.One2many('maintenance.transform', 'equipment_id', string='Transforms')
+    metric_ids = fields.One2many('maintenance.metric', 'equipment_id', string='Metrics')
