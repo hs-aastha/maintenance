9e0c13314fa8bdd02a2cd171f73e60c351b6594f
hs-aastha <aastha.a@hundredsolutions.com>
2024-07-31 12:15:45 +0530

    Adding buttons in equipment form view

--- sitewise_integration/models/maintenance_equipment.py
+++ sitewise_integration/models/maintenance_equipment.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class MaintenanceEquipment(models.Model):
     _inherit = 'maintenance.equipment'
@@ -8,12 +8,12 @@ class MaintenanceEquipment(models.Model):
     transform_ids = fields.One2many('maintenance.transform', 'equipment_id', string='Transforms')
     metric_ids = fields.One2many('maintenance.metric', 'equipment_id', string='Metrics')
 
+    @api.model
     def action_create_model(self):
-        # Placeholder method for 'Create Model' button
-        # Add your functionality here
-        return
+        # Placeholder method for creating a model
+        pass
 
+    @api.model
     def action_create_asset(self):
-        # Placeholder method for 'Create Asset' button
-        # Add your functionality here
-        return
+        # Placeholder method for creating an asset
+        pass
