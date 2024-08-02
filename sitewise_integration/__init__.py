5d76c423d8b26140565326cf3a2617f6db890af2
hs-aastha <aastha.a@hundredsolutions.com>
2024-08-02 16:04:13 +0530

    Update __init__.py to include lib directories

--- sitewise_integration/__init__.py
+++ sitewise_integration/__init__.py
@@ -1,9 +1,22 @@
 import sys
 import os
+import site
 
-# Add the 'lib' directory to the Python path
-lib_path = os.path.join(os.path.dirname(__file__), 'lib')
-if lib_path not in sys.path:
-    sys.path.append(lib_path)
+# Logging setup to debug issues with loading libraries
+import logging
+_logger = logging.getLogger(__name__)
+
+# Path to the lib directory
+lib_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'lib')
+
+# Adding each subdirectory in lib_path to sys.path
+for subdir in os.listdir(lib_path):
+    full_subdir_path = os.path.join(lib_path, subdir)
+    if os.path.isdir(full_subdir_path):
+        site.addsitedir(full_subdir_path)
+        sys.path.append(full_subdir_path)
+
+_logger.info("Added lib_path to sys.path: %s", lib_path)
+_logger.info("Current sys.path: %s", sys.path)
 
 from . import models
