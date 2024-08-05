b3ac08efd6ee5587c003c6b0c685dfe20e64dcd8
hs-aastha <aastha.a@hundredsolutions.com>
2024-08-05 16:10:02 +0530

    Update AWS credentials integration

--- sitewise_integration/models/res_config_settings.py
+++ sitewise_integration/models/res_config_settings.py
@@ -1,31 +1,9 @@
-import logging
 from odoo import models, fields, api
-
-_logger = logging.getLogger(__name__)
+import boto3
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    aws_access_key_id = fields.Char(string='AWS Access Key ID', config_parameter='sitewise_integration.aws_access_key_id')
-    aws_secret_access_key = fields.Char(string='AWS Secret Access Key', config_parameter='sitewise_integration.aws_secret_access_key')
-    aws_region = fields.Char(string='AWS Region', config_parameter='sitewise_integration.aws_region')
-
-    @api.model
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        res.update(
-            aws_access_key_id=self.env['ir.config_parameter'].sudo().get_param('sitewise_integration.aws_access_key_id'),
-            aws_secret_access_key=self.env['ir.config_parameter'].sudo().get_param('sitewise_integration.aws_secret_access_key'),
-            aws_region=self.env['ir.config_parameter'].sudo().get_param('sitewise_integration.aws_region'),
-        )
-        _logger.info(f"Get AWS Configurations: {res}")
-        return res
-
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        self.env['ir.config_parameter'].sudo().set_param('sitewise_integration.aws_access_key_id', self.aws_access_key_id)
-        self.env['ir.config_parameter'].sudo().set_param('sitewise_integration.aws_secret_access_key', self.aws_secret_access_key)
-        self.env['ir.config_parameter'].sudo().set_param('sitewise_integration.aws_region', self.aws_region)
-        _logger.info(f"Set AWS Access Key ID: {self.aws_access_key_id}")
-        _logger.info(f"Set AWS Secret Access Key: {self.aws_secret_access_key}")
-        _logger.info(f"Set AWS Region: {self.aws_region}")
+    aws_access_key_id = fields.Char(string='AWS Access Key ID', config_parameter='sitewise_integration.aws_access_key_id', help='Enter your access key here.')
+    aws_secret_access_key = fields.Char(string='AWS Secret Access Key', config_parameter='sitewise_integration.aws_secret_access_key', help='Enter your secret key here.')
+    aws_region = fields.Char(string='AWS Region', config_parameter='sitewise_integration.aws_region', help='Enter your region here.')
