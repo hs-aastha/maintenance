083dea63c459382418b7b48c9b28e77878dff013
hs-arjun <arjun.chandra.baidya@gmail.com>
2024-09-02 12:23:57 +0600

    updated

--- sitewise_integration/models/maintenance_equipment_category_inherit.py
+++ sitewise_integration/models/maintenance_equipment_category_inherit.py
@@ -5,4 +5,7 @@ class MaintenanceEquipmentCategory(models.Model):
     _inherit = 'maintenance.equipment.category'
 
     sitewise_model_id = fields.Char(string='SiteWise Model ID')
-
+    attribute_ids = fields.One2many('maintenance.attribute', 'equipment_category_id', string='Attributes')
+    measurement_ids = fields.One2many('maintenance.measurement', 'equipment_category_id', string='Measurements')
+    transform_ids = fields.One2many('maintenance.transform', 'equipment_category_id', string='Transforms')
+    metric_ids = fields.One2many('maintenance.metric', 'equipment_category_id', string='Metrics')
