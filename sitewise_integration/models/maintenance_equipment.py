879e125e75b152d0e94cab4e9aede4159878b708
hs-arjun <arjun.chandra.baidya@gmail.com>
2024-09-03 16:29:00 +0600

    equipment line view update

--- sitewise_integration/models/maintenance_equipment.py
+++ sitewise_integration/models/maintenance_equipment.py
@@ -16,6 +16,19 @@ class MaintenanceEquipment(models.Model):
     sitewise_model_id = fields.Char(string='SiteWise Model ID')
     sitewise_asset_id = fields.Char(string='SiteWise Asset ID')
 
+    @api.onchange('category_id')
+    def onchange_data(self):
+        if self.category_id:
+            self.attribute_ids = self.category_id.maintenance_attribute_line_ids
+            self.measurement_ids = self.category_id.maintenance_measurement_line_ids
+            self.transform_ids = self.category_id.maintenance_transform_line_ids
+            self.metric_ids = self.category_id.maintenance_metric_line_ids
+        else:
+            self.attribute_ids = [(5, 0, 0)]
+            self.measurement_ids = [(5, 0, 0)]
+            self.transform_ids = [(5, 0, 0)]
+            self.metric_ids = [(5, 0, 0)]
+
     def get_aws_client(self, service_name):
         aws_access_key_id = self.env['ir.config_parameter'].sudo().get_param('sitewise_integration.aws_access_key_id')
         aws_secret_access_key = self.env['ir.config_parameter'].sudo().get_param(
@@ -34,27 +47,27 @@ class MaintenanceEquipment(models.Model):
             aws_secret_access_key=aws_secret_access_key
         )
 
-    def create_sitewise_model(self):
-        client = self.get_aws_client('iotsitewise')
-
-        asset_model_payload = {
-            "assetModelName": "OdooModel",
-            "assetModelDescription": "Model created from Odoo",
-            "assetModelProperties": [
-                {
-                    "name": "Property1",
-                    "dataType": "STRING",
-                    "type": {
-                        "attribute": {
-                            "defaultValue": "DefaultValue"
-                        }
-                    }
-                }
-            ]
-        }
-
-        response = client.create_asset_model(**asset_model_payload)
-        return response
+    # def create_sitewise_model(self):
+    #     client = self.get_aws_client('iotsitewise')
+    #
+    #     asset_model_payload = {
+    #         "assetModelName": "OdooModel",
+    #         "assetModelDescription": "Model created from Odoo",
+    #         "assetModelProperties": [
+    #             {
+    #                 "name": "Property1",
+    #                 "dataType": "STRING",
+    #                 "type": {
+    #                     "attribute": {
+    #                         "defaultValue": "DefaultValue"
+    #                     }
+    #                 }
+    #             }
+    #         ]
+    #     }
+    #
+    #     response = client.create_asset_model(**asset_model_payload)
+    #     return response
 
     def create_sitewise_asset(self):
         client = self.get_aws_client('iotsitewise')
@@ -67,12 +80,12 @@ class MaintenanceEquipment(models.Model):
         response = client.create_asset(**asset_payload)
         return response
 
-    def button_create_model(self):
-        for record in self:
-            response = record.create_sitewise_model()
-            if response and 'assetModelId' in response:
-                record.sitewise_model_id = response['assetModelId']
-            # You can add further logic to handle the response if needed
+    # def button_create_model(self):
+    #     for record in self:
+    #         response = record.create_sitewise_model()
+    #         if response and 'assetModelId' in response:
+    #             record.sitewise_model_id = response['assetModelId']
+    #         # You can add further logic to handle the response if needed
 
     def button_create_asset(self):
         for record in self:
