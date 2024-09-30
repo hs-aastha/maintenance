592766df75ed40462839adbc37f9942ab36bc43c
hs-arjun <arjun.chandra.baidya@gmail.com>
2024-09-30 17:02:01 +0600

    updated transform

--- sitewise_integration/models/maintenance_equipment_category_inherit.py
+++ sitewise_integration/models/maintenance_equipment_category_inherit.py
@@ -151,13 +151,17 @@ class MaintenanceEquipmentCategory(models.Model):
                 property_dict["externalId"] = measurement_line.external_id
             asset_model_properties.append(property_dict)
             # property_name_to_id = measurement_line.name.name
-            property_name_to_id = re.sub(r'[^a-z0-9_]', '_', measurement_line.name.name)
-        # Process maintenance_transform_line_ids
+            property_name_to_id = re.sub(r'[^a-z0-9_]', '_', measurement_line.name.name.lower())
+            if property_name_to_id.startswith('_'):
+                property_name_to_id = 'a' + property_name_to_id[1:]
+                # Process maintenance_transform_line_ids
         for transform_line in self.maintenance_transform_line_ids:
             if transform_line.data_type.upper() not in ['DOUBLE', 'STRING']:
                 raise ValidationError(
                     f"Invalid data type '{transform_line.data_type}' for transform '{transform_line.name.name}'. Must be 'DOUBLE' or 'STRING'.")
-            sanitized_property_name = re.sub(r'[^a-z0-9_]', '_', property_name_to_id)
+            sanitized_property_name = re.sub(r'[^a-z0-9_]', '_', property_name_to_id.lower())
+            if sanitized_property_name.startswith('_'):
+                sanitized_property_name = 'a' + sanitized_property_name[1:]
             property_dict = {
                 "name": transform_line.name.name,
                 "dataType": transform_line.data_type.upper(),
